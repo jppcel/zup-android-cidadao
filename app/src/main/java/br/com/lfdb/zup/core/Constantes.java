bd8192611c79ef07ce6ffe5ee3a8d15effa0eefc
João Paulo Polles <joaopaulopolles@gmail.com>
2017-10-12 23:28:52 -0300

    update

--- app/src/main/java/br/com/lfdb/zup/core/Constantes.java
+++ app/src/main/java/br/com/lfdb/zup/core/Constantes.java
@@ -6,11 +6,11 @@ public class Constantes {
 	public static final String PREF_KEY_OAUTH_TOKEN = "oauthToken";
     public static final String PREF_KEY_OAUTH_SECRET = "oauthTokenSecret";
     
-    public static final String REST_URL = "http://particity.cascavel.pr.gov.br:8282";
-    public static final String WEBSITE_URL = "http://particity.cascavel.pr.gov.br";
-    public static final String PLACES_KEY = "AIzaSyDfTNtL1I0r7MLSa3wW";
+    public static final String REST_URL = "http://zup.mcr.pr.gov.br:8282";
+    public static final String WEBSITE_URL = "http://zup.mcr.pr.gov.br";
+    public static final String PLACES_KEY = "AIzaSyA9NA1HlV3DKiUjvwaE5Ot8N689Swwxmks";
 
     public static final String NAMESPACE_DEFAULT = "1";
-    public static final double INITIAL_LATITUDE = -24.9580531;
-    public static final double INITIAL_LONGITUDE = -53.4677844;
+    public static final double INITIAL_LATITUDE = -24.5581316;
+    public static final double INITIAL_LONGITUDE = -54.057559;
 }
